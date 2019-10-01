eb095d2ae407c95d676cfd385ee69cbb0c6d577e
Faber <fabriziocasadei97@gmail.com>
2019-10-01 18:22:23 +0200

    Modified the template for the indexing, now ti shows which the sysytem
    is creating indexing files

--- Media-SearchEngineWeb/src/main/resources/static/javaScript/startIndexing.js
+++ Media-SearchEngineWeb/src/main/resources/static/javaScript/startIndexing.js
@@ -2,11 +2,16 @@ $(document).ready(function(){
 	  $("#indexB").click(function(){
 	    $.ajax({
 	      url : 'Indexing',
+	      beforeSend: function (){
+	    	  $("#loading").html("Indicizzazione in corso...");
+	      },
 	      success: function (data){
 	        window.alert(data);
+	        $("#loading").remove();
 	      },
 	      error : function(){
 	    	  window.alert("Errore nell'indicizzazione");
+	    	  $("#loading").remove();    	
 	      }
 	    });
 	  });
