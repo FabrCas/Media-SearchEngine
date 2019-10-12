38a8a4168792c164096aece87bb125df63cb475c
Faber <fabriziocasadei97@gmail.com>
2019-10-12 15:46:05 +0200

    Started the implementation of the creation of areas on document with
    pointer on, over the areas of the hits

--- Media-SearchEngineWeb/src/main/resources/static/javaScript/vizinfo.js
+++ Media-SearchEngineWeb/src/main/resources/static/javaScript/vizinfo.js
@@ -1,6 +1,5 @@
-//differenza tra il mio codice e quello di partenza, inizialmente voglio creare aree
-//SOLO per le parole trovate dalla ricerca e non tutte
-function createAreas(div_id, image_path, map_name, page_name, coordD, coordP ) {
+//funzione per evidenziare le hit della ricerca
+function createAreasHit(div_id, image_path, map_name, page_name, coordD, coordP ) {
   var root_div = document.getElementById(div_id); //div della pagina documento.html,
                                                   //  dove verrà caricata l'immagine
   var map_ele = document.createElement("map");
@@ -10,7 +9,7 @@ function createAreas(div_id, image_path, map_name, page_name, coordD, coordP ) {
 
   map_ele.name = map_name;
   img_ele.src = image_path;
-  img_ele.classList.add('map'); //utilizzo questa classe con maplight
+  img_ele.classList.add('mapHit'); //utilizzo questa classe con maplight
   img_ele.useMap = "#" + map_name;  //attributo necessario al plugin maphilight
 
   var pagename = page_name; /*prende page? */
@@ -30,7 +29,6 @@ function createAreas(div_id, image_path, map_name, page_name, coordD, coordP ) {
   console.log("y doc: "+abs_y);
  //ottengo oggetto contenente le coppie chiave valore dei box in cui c'è stato matching
   var page_bbxs = coordP;
-  console.log(page_bbxs.length);
   
 /*sttuttura del file
 "numPag_xAreaTesto_yAreaTesto_larghezzaAreaTesto_altezzaAreaTesto": {
@@ -62,13 +60,78 @@ var lunghezza= page_bbxs.length;
   position to the top.
   #->primo elemento*/
       area_ele.href = "#";
-      area_ele.title = page_bbxs[i];
 
       map_ele.appendChild(area_ele);
   }
 
+
   root_div.appendChild(map_ele);
   root_div.appendChild(img_ele);
+  //createAreas(root_div, image_path,img_ele, map_name, map_ele);
 }
 
 // $("area").filter( function(i){ return $(this).attr( "title" ).includes("latin") }).data('maphilight', {alwaysOn: false}).trigger('alwaysOn.maphilight');
+
+//funzione per evidenziare e avere info delle trascrizioni passando il cursore sopra i boxes
+function createAreas(root_div, image_path, img_ele ,map_name, map_ele1) {
+	
+	 var map_ele = document.createElement("map");
+	  img_ele.classList.add('map');
+
+	  var pagename = image_path.split('/')[2].split('.')[0]; /*prende page? */
+	  console.log(pagename);
+	  /*bbxs variabile in scripts/gen_transcriptions_nodup.js*/
+	  /*Il costruttore Object crea un oggetto avente il valore dato. Se il valore è
+	  null o undefined, verrà creato un oggetto vuoto; altrimenti un oggetto del tipo
+	  corrispondente al valore dato. Se il valore è già un oggetto, verra restituito
+	  senza alcuna modifica.
+	  /*Object.keys() Restituisce un array contenente i nomi di tutte le proprietà
+	   enumerabili dell'oggetto.*/
+	  var pagekey = Object.keys(bbxs).filter(
+	    function(k, i){ return k.startsWith(pagename) })[0];
+	    /*ottengo la posizione x dell' area di testo*/
+	  var abs_x = Number(pagekey.split('_')[1]);  //Number() costrutor, number-> numeric data typer 64 bit
+	  /*ottengo la posizione x dell' area di testo*/
+	  var abs_y = Number(pagekey.split('_')[2]);
+	  var page_bbxs = bbxs[pagekey]; //ottengo oggetto contenente le coppie chiave valore dei box
+
+	/*sttuttura del file
+	"numPag_xAreaTesto_yAreaTesto_larghezzaAreaTesto_altezzaAreaTesto": {
+	    "xBoxParola_yBoxParola_larghezzaBoxParola_altezzaBoxParola": "trascrizione1\ntrascrizione2\ntrascrizione3",
+	*/
+
+
+	  Object.keys(page_bbxs).forEach(
+	    function(k, i){
+	      /*costruisco le coordinate*/
+	      var coords = k.split('_');
+	      var x1 = Number(coords[0]) + abs_x; //xBox parola+ xAreaTesto
+	      var y1 = Number(coords[1]) + abs_y;
+	      var x2 = x1 + Number(coords[2]);
+	      var y2 = y1 + Number(coords[3]);  //angolo opposto all'origine
+
+	      /*creo un nuovo tag <area> con i valori trovati*/
+	      var area_ele = document.createElement("area");
+	      area_ele.shape = 'rect';
+	      area_ele.coords = x1+","+y1+","+x2+","+y2;
+
+	/*The main use of anchor tags - <a></a> - is as hyperlinks. That basically means
+	 that they take you somewhere. Hyperlinks require the href property, because it
+	  specifies a location.
+	  href="#" doesn't specify an id name, but does have a corresponding location -
+	  the top of the page. Clicking an anchor with href="#" will move the scroll
+	  position to the top.
+	  #->primo elemento*/
+	      area_ele.href = "#";
+	      area_ele.title = page_bbxs[k]
+
+	      map_ele.appendChild(area_ele);
+	    }
+	  );
+
+	  root_div.appendChild(map_ele1);
+	  root_div.appendChild(map_ele);
+	  root_div.appendChild(img_ele);
+	}
+
+	// $("area").filter( function(i){ return $(this).attr( "title" ).includes("latin") }).data('maphilight', {alwaysOn: false}).trigger('alwaysOn.maphilight');
